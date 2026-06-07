df21464d2bf0589e6a3cc4e558403ad2b18da335
Marvin Hofmann <pb63memo@studserv.uni-leipzig.de>
2026-06-07 21:08:10 +0200

    add comment for crontab, add debug channel, add notification, remove infinite loop

--- fetch_and_send.py
+++ fetch_and_send.py
@@ -1,6 +1,9 @@
 import requests
 import time
 
+
+# script should be used in a crontab like this:
+# */15 * * * * /usr/bin/python3 /path/to/script/fetch_and_send.py
 url = "https://app.cituro.com/bookingService"
 
 headers = {
@@ -25,6 +28,13 @@ payloads = [
     "payload_50min.txt",
     "payload_hmt.txt"]
 
+"""
+wenn es keine Termine gibt, dann siehts so aus:
+//OK[0,1,["java.util.ArrayList/4159755760"],0,7]
+
+wenn es welche gibt, dann so:
+//OK[21,-12,2026,0,8,0,11,11,14,0,0,16,23,2026,0,8,50,11,11,14,0,0,"Z6HmxRg",13,"CX7",0,0,0,0,0,"Z_wDNKA",13,-6,1,8,0,0,0,0,0,0,0,0,7,0,22,5,0,-4,0,0,3,0,2,21,-12,2026,0,8,0,13,4,14,0,0,16,20,2026,0,8,50,13,4,14,0,0,"Z6Hmp8w",13,"CX7",0,0,0,0,0,"Z_MbiuA",13,-6,1,8,0,0,0,0,0,0,0,0,7,0,19,5,0,-4,0,0,3,0,2,18,0,17,2026,0,8,0,9,4,14,0,0,16,15,2026,0,8,50,9,4,14,0,0,"Z5tVRy4",13,"CX7",0,0,0,0,0,"Z_LknGA",13,0,12,11,0,10,9,1,8,0,0,0,0,0,0,0,0,7,0,6,5,0,10,4,0,0,3,0,2,3,1,["java.util.ArrayList/41597557
+# """
 def termin_available(response):
     if len(response.text) > 48:
         return True
@@ -39,14 +49,14 @@ def send_msg_to_ntfy(msg, topic):
 
 
 def main():
-    while True:
+    try:
         with open(payloads[0], "r") as f:
             payload = f.read()
         response = requests.post(url, headers=headers, data=payload)
         if termin_available(response):
             print("sending notification for kurzberatung")
             send_msg_to_ntfy("Kurzberatung verfügbar!", "psychosoziale_kurzberatung")
-    
+
         with open(payloads[1], "r") as f:
             payload = f.read()
         response = requests.post(url, headers=headers, data=payload)
@@ -60,9 +70,9 @@ def main():
         if termin_available(response):
             print("sending notification for erstgeprächstermin (HMT)")  
             send_msg_to_ntfy("Erstgeprächstermin (HMT) verfügbar!", "psychosoziale_beratung_hmt")
-        
-        
-        time.sleep(15 * 60)  # Wait for 60 seconds before checking again
-    
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        send_msg_to_ntfy(f"An error occurred: {e}", "psychosoziale_beratung_debug")
+
 if __name__ == "__main__":
     main()
